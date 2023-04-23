3a91f334e546a9299386ac28043ca2ec58603210
charlocharlie <charlielaabs@gmail.com>
2023-04-23 18:07:57 -0500

    Remove navigateFallback

--- rollup.config.js
+++ rollup.config.js
@@ -68,7 +68,6 @@ export default {
     /** Create and inject a service worker */
     generateSW({
       globIgnores: ['polyfills/*.js', 'nomodule-*.js'],
-      navigateFallback: '/index.html',
       // where to output the generated sw
       swDest: path.join('dist', 'sw.js'),
       // directory to match patterns against to be precached
